ebf5800c2d9f00c0f07e6bc3b7be7d7a78a08108
kyeimuda <kyeibernard01@gmail.com>
2022-02-01 17:42:24 -0800

    test for base_model

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -4,23 +4,55 @@
 import unittest
 from models.base_model import BaseModel
 
+
 class TestBaseModel(unittest.TestCase):
     """ Unittest for BaseModel class"""
-    
-    def test_id(self):
-        """testing to check if self.id is no equal """
-        test_try2 = BaseModel()
-        test_try1 = BaseModel()
-        self.assertNotEqual(test_try2.id, test_try1.id)
 
     def test_init_BaseModel(self):
-        """testing to check if an object is of type BaseModel"""
-        test_try = BaseModel()
-        self.assertIsInstance(test_try, BaseModel)
+        """test if an object is an type BaseModel"""
+        my_object = BaseModel()
+        self.assertIsInstance(my_object, BaseModel)
+
+    def test_id(self):
+        """ test that id is unique """
+        my_objectId = BaseModel()
+        my_objectId1 = BaseModel()
+        self.assertNotEqual(my_objectId.id, my_objectId1.id)
 
     def test_str(self):
-        """testing to check if __str__ returns the rigth string format"""
-        test_try = BaseModel()
-        string1 ="[{}] ({}) {}".format(test_try.__class__.__name__, self.id, self.__dict__)
+        '''check if the output of str is in the specified format'''
+        my_strobject = BaseModel()
+        _dict = my_strobject.__dict__
+        string1 = "[BaseModel] ({}) {}".format(my_strobject.id, _dict)
         string2 = str(my_strobject)
         self.assertEqual(string1, string2)
+
+    def test_save(self):
+        """ check if date update when save """
+        my_objectupd = BaseModel()
+        first_updated = my_objectupd.updated_at
+        my_objectupd.save()
+        second_updated = my_objectupd.updated_at
+        self.assertNotEqual(first_updated, second_updated)
+
+    def test_to_dict(self):
+        '''check if to_dict returns a dictionary, if add a class
+        key with class name of the object and if updated_at and
+        created_at are converted to string object in ISO format.'''
+        my_model3 = BaseModel()
+        my_dict_model3 = my_model3.to_dict()
+        self.assertIsInstance(my_dict_model3, dict)
+        for key, value in my_dict_model3.items():
+            flag = 0
+            if my_dict_model3['__class__'] == 'BaseModel':
+                flag += 1
+            self.assertTrue(flag == 1)
+        for key, value in my_dict_model3.items():
+            if key == 'created_at':
+                self.assertIsInstance(value, str)
+            if key == 'updated_at':
+                self.assertIsInstance(value, str)
+
+
+if __name__ == '__main__':
+    unittest.main()
