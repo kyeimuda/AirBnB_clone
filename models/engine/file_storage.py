aafe3ca0f030cf5001a8ab68808beb54e37a80a9
kyeimuda <kyeibernard01@gmail.com>
2022-02-01 19:00:30 -0800

    file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,6 +6,12 @@ file_storage model that serializes python objects and deserialize json files
 
 import json
 from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 class FileStorage:
@@ -63,7 +69,6 @@ class FileStorage:
 
         try:
             with open(self.__file_path, 'r', encoding="UTF8") as f:
-                # jlo = json.load(f)
                 for key, value in json.load(f).items():
                     attri_value = eval(value["__class__"])(**value)
                     self.__objects[key] = attri_value
