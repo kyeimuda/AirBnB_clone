fe3cdeeec30d155f871a95beb73ee1588e1a327d
kyeimuda <kyeibernard01@gmail.com>
2022-02-06 09:45:44 -0800

    base_model constructor correction

--- models/base_model.py
+++ models/base_model.py
@@ -22,11 +22,11 @@ class BaseModel:
                     value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                 if key != "__class__":
                     setattr(self, key, value)
-                else:
-                    self.id = str(uuid.uuid4())
-                    self.created_at = datetime.now()
-                    self.updated_at = datetime.now()
-                    models.storage.new(self)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """representation of a string"""
