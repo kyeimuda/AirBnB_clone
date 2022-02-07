3da18b4d7049bf0629349a0ab8bca908fd086131
kyeimuda <kyeibernard01@gmail.com>
2022-02-06 19:15:34 -0800

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -26,11 +26,11 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            #models.storage.new(self)
 
     def __str__(self):
         """representation of a string"""
-        return "[{}] ({}) {}".format(self.__class__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """Updates public instance attribute with the current time"""
@@ -40,7 +40,7 @@ class BaseModel:
     def to_dict(self):
         """Returns a dictionary containing all key values of  an instance"""
         dict_copy = self.__dict__
-        dict_copy["__class__"] = self.__class__
+        dict_copy["__class__"] = self.__class__.__name__
         dict_copy["created_at"] = self.created_at.isoformat()
         dict_copy["updated_at"] = self.updated_at.isoformat()
         return dict_copy
